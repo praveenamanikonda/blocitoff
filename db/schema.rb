7afa7f4c776b5664a68dac3303fe16778b448da6
praveenamanikonda <praveena.manikonda@gmail.com>
2015-02-22 22:59:49 +0000

    Created Lists model and lists controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117165938) do
+ActiveRecord::Schema.define(version: 20150222090022) do
+
+  create_table "lists", force: true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "lists", ["user_id"], name: "index_lists_on_user_id"
 
   create_table "todos", force: true do |t|
     t.string   "description"
