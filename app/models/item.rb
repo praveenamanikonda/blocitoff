fc73e9587a8e13c153a2d3b499ca464bc9c7361d
praveenamanikonda <praveena.manikonda@gmail.com>
2015-03-05 19:23:59 +0000

    Added days left feature and some styling

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :list
   
   def days_left
-    (DateTime.now.to_date - created_at.to_date).to_i
+#     (DateTime.now.to_date - created_at.to_date).to_i
+    days_left = (created_at + 7.days)
   end 
   
 end
